eb083a1571c15f494c55095b9641add2886cf967
Matheus Martins <79486272+Matheus-Martins13@users.noreply.github.com>
2021-12-21 19:57:13 -0300

    Update index.js

--- diferenca_var_let/js/index.js
+++ diferenca_var_let/js/index.js
@@ -5,6 +5,7 @@
  * já com o let, isso gera um erro. O recomendável é utilizar o let.
  * Ao se declarar uma variável sem o var ou o let, estamos declarando uma variável global,
  * o que pode causar erros severos dentro do seu programa. 
+ * A variável let possui escopo de bloco, a variável var possui escopo de função.
 */
 
 var nome = 'Luiz';
